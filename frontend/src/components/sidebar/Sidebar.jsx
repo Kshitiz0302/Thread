97710d3f88de17af690ed8369a043c2b0bb770e8
Kshitiz <kshitizdhuria@gmail.com>
2024-08-23 08:08:45 +0530

    Making UI responsive to mobile-end

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
-const Sidebar = (isMobile) => {
+const Sidebar = () => {
 	return (
 		
 		<div className='border-r border-slate-500 p-4 flex flex-col '>
